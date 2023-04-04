80e6e8f7030ac56f3a16d2d7c30df1229700c65a
AadityaJujagar <aadityajujagar@gmail.com>
2023-04-04 12:58:06 +0530

    main function, cin, cout, variable learnt

---
+++ CPP/main_function_day1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// endl, \n, cout, cin, int var
+
+main()
+{
+    // cout and endl statements
+    cout << "Namaste Bharat!" << endl;
+    // use of \n for the next line
+    cout << "first line\nnow on the next line\n";
+    // declaring int variables, use of cin
+    int var1;
+    cin >> var1;
+    cout << "variable number 1 = " << var1;
+}
